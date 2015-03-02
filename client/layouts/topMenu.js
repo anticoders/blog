e12994b612361655d95999c4df6600c199e8d3dc
Tomasz Lenarcik <apendua@gmail.com>
2015-03-02 13:39:04 +0100

    Animated top banner

--- client/layouts/topMenu.js
+++ client/layouts/topMenu.js
@@ -1,11 +1,14 @@
 
 Template.topMenu.rendered = function () {
-  
+
 }
 
 Template.topMenu.helpers({
   activeIf: function (name) {
     return Helpers.routeIs(name) ? 'active' : '';
+  },
+  hiddenIf: function (name) {
+    return Helpers.routeIs(name) ? 'hidden' : '';
   },
   hint: function () {
     var controller = Iron.controller();
