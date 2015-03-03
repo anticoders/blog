b0712d2d82a7eefb3157d94aa350a55682ad38bd
matikucharski <mati.kucharski@gmail.com>
2015-03-03 17:19:59 +0100

    added sorting option for chunks

--- client/views/blogPostEdit.js
+++ client/views/blogPostEdit.js
@@ -3,7 +3,27 @@ Template.blogPostEdit.created = function () {
 };
 
 Template.blogPostEdit.rendered = function () {
+  var that = this;
 
+  // init sortable plugin with sort events
+  this.$('.chunks-container').sortable({
+    axis: 'y',
+    handle: '.sort-chunk-handler',
+    items: '> .chunkEditor',
+    start: function(e, ui) {
+      that.indexBeforeSort = ui.item.index();
+    },
+    stop: function(e, ui) {
+      var indexAfterSort = ui.item.index();
+      var chunkArray = that.data.chunks;
+      Helpers.moveInArray(chunkArray, that.indexBeforeSort, indexAfterSort); //move element at new index in-place
+      BlogPosts.update({ _id: that.data._id }, { $set: { chunks: chunkArray } });
+    }
+  });
+};
+
+Template.blogPostEdit.destroyed = function () {
+  this.$('.chunks-container').sortable('destroy');
 };
 
 Template.blogPostEdit.events({
