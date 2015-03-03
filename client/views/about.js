3395e16b5d8fbe15a3e601044cf2b89ea5286619
Tomasz Lenarcik <apendua@gmail.com>
2015-03-03 16:29:52 +0100

    Added "bio" component

--- client/views/about.js
+++ client/views/about.js
@@ -6,4 +6,16 @@ Template.about.helpers({
   lorem: function () {
     return Fake.paragraph(20);
   },
+  bio: function () {
+    return Fake.paragraph(5) + '\n\n' + Fake.paragraph(10);
+  },
+  name: function () {
+    return Fake.user().fullname;
+  },
+  motto: function () {
+    return Fake.sentence();
+  },
+  members: function () {
+    return [ { index: 1 }, { index: 8 }, { index: 7 } ];
+  },
 });
