a0f05ac7f933b14a341ef9aa06bc273b08453473
apendua <apendua@gmail.com>
2015-03-05 18:53:24 +0100

    Added basic framework for modals

---
+++ client/lib/modal.js
@@ -0,0 +1,43 @@
+
+App.modal = function (name, data, options) {
+  
+  var view = Blaze.renderWithData(Template[name], {}, $('body').get(0));
+
+  view._modal = options || {};
+
+  var onHide = view._modal.onHide;
+
+  view._modal.onHide = function () {
+    // first call the user provided callback
+    onHide && onHide.apply(this, arguments);
+
+    // wait until all animations are done
+    Meteor.setTimeout(function () {
+      // and finally destroy the view
+      Blaze.remove(view);
+    }, 1000);
+  }
+
+  return view;
+}
+
+App.showModal = function (node) {
+  var view = findView();
+  if (!view) {
+    return;
+  }
+  $(node).modal(view._modal);
+  $(node).modal('show');
+}
+
+function findView() {
+  var view = Blaze.currentView;
+  while (view) {
+    if (view) {
+      if (!!view._modal) {
+        return view;
+      }
+    }
+    view = view.parentView;
+  }
+}
