8295ed98a2cd0bb19df2bf6e0476bc8a53eabfa3
apendua <apendua@gmail.com>
2015-03-05 11:07:28 +0100

    User can now update their profile info

--- client/views/profile.js
+++ client/views/profile.js
@@ -3,6 +3,13 @@ Template.profile.events({
   'submit .ui.form': function (e, t) {
     e.preventDefault();
     var data = t.$('.ui.form').form('get values');
-    console.log('submit', data);
+    var updates = {};
+    _.each(data, function (value, key) {
+      updates['profile.' + key] = value;
+    });
+    if (_.isEmpty(updates)) {
+      return;
+    }
+    Meteor.users.update({ _id: Meteor.userId() }, { $set: updates });
   },
 });
