0bad7c9dd3f6ca86d5c3ef6deac494ceee066e0f
apendua <apendua@gmail.com>
2015-03-05 13:45:22 +0100

    Users who are not logged in can only see published data

--- server/publications.js
+++ server/publications.js
@@ -4,5 +4,31 @@ Meteor.publish(null, function () {
 });
 
 Meteor.publish('blogPosts', function () {
-  return BlogPosts.find({});
+  var self = this, handle;
+
+  if (!self.userId) {
+
+    // pipe items from Published collection to client "blogPosts" sink
+
+    handle = Published.find({}).observeChanges({
+      added: function (id, fields) {
+        self.added('blogPosts', id, fields);
+      },
+      changed: function (id, fields) {
+        self.changed('blogPosts', id, fields);
+      },
+      removed: function (id) {
+        self.removed('blogPosts', id);
+      },
+    });
+
+    self.ready();
+
+    self.onStop(function () {
+      handle.stop();
+    });
+
+  } else {
+    return BlogPosts.find({});
+  }
 });
