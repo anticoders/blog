b772d7f0f2f6826b0b33899accdbcf3f769c7cfa
apendua <apendua@gmail.com>
2015-03-05 18:55:26 +0100

    Improved scrollTop compatibility

--- client/main.js
+++ client/main.js
@@ -5,7 +5,8 @@ Template.body.rendered = function () {
 
 Template.body.events({
   'click [data-action=scrollTop]': function () {
-    $('body').scrollTop(0);
+    // 'body' will not work in all browsers ...
+    $(window).scrollTop(0);
   },
   'click [data-action=logout]': function () {
     Meteor.logout();
