7722859f57858f8567bb5ab82dfdc023b9470b50
apendua <apendua@gmail.com>
2015-03-06 17:18:38 +0100

    Secure updates on BlogPost collection

--- server/privileges.js
+++ server/privileges.js
@@ -1,9 +1,22 @@
 
 BlogPosts.allow({
+  insert: anyoneLoggedIn,
+  remove: onlyTheOwner,
+  update: onlyTheOwner,
+});
 
+// these documents can be only accessed with custom methods
+
+Published.deny({
   insert: Utils.constant(true),
-
   remove: Utils.constant(true),
-
   update: Utils.constant(true),
 });
+
+function anyoneLoggedIn(userId) {
+  return !!userId;
+}
+
+function onlyTheOwner(userId, doc) {
+  return !!userId && doc.createdBy === userId;
+}
