006ab2710a1950f598349f872fda0ff697acef6d
apendua <apendua@gmail.com>
2015-03-10 10:59:53 +0100

    Resolved problems with sortable plugin

--- client/components/navigation/sidebar.js
+++ client/components/navigation/sidebar.js
@@ -1,3 +1,22 @@
+Template.sidebar.rendered = function () {
+
+  // this is an ugly fix, but we need it because due to semantic-ui
+  // incompatibility with jquery sortable plugin; briefly speaking
+  // setting "overflow-x = hidden" on the "body" element prevents
+  // the plugin from computing offsets properly
+
+  this.$('.sidebar').sidebar({
+    onVisible: function () {
+      // we need to use both body and html to make sure
+      // it also works with "the other" browsers
+      $('body, html').addClass('overflow-x-hidden');
+    },
+    onHidden: function () {
+      $('body, html').removeClass('overflow-x-hidden');
+    },
+  });
+}
+
 Template.sidebar.events({
   'click [data-action=toggle]': function (e, t) {
     t.$('.sidebar').sidebar('toggle');
--- client/main.js
+++ client/main.js
@@ -1,12 +1,12 @@
 
 Template.body.rendered = function () {
-  $('body').addClass('dimmable pushable');
+  $('body').addClass('dimmable pushable scrollable');
 }
 
 Template.body.events({
   'click [data-action=scrollTop]': function () {
     // 'body' will not work in all browsers ...
-    $(window).scrollTop(0);
+    $(document).scrollTop(0);
   },
   'click [data-action=logout]': function () {
     Meteor.logout();
